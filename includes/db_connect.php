65585116a184ebf7e7cafc9be31289d7e817b912
luong2004 <125137391+levanluong2k4@users.noreply.github.com>
2025-11-28 12:34:47 +0700

    Modify database connection settings
    Updated database connection parameters to use default port and empty password.

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -3,11 +3,8 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$ketnoi = mysqli_connect("localhost:8889", "root", "root", "php_pizza");
 
-if (!$ketnoi) {
-    die("Kết nối thất bại: " . mysqli_connect_error());
-}
-
+$ketnoi = mysqli_connect("localhost", "root", "", "php_pizza");
 mysqli_set_charset($ketnoi, "utf8");
+
 ?>
